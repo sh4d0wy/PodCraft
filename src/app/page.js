92161f1695a6b0e7120dceb5397f0e7fa0bad7db
sh4d0wy <sakshambhugra8@gmail.com>
2023-11-28 14:46:48 +0530

    changed some styling

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,12 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-5xl bold-500'>PodCraft</h1>
+      <div className="h-[95vh] absolute w-[25vw] opacity-50 left-3 top-5 z-100 bg-[#0E172B] outline rounded-md">
+      </div>
+        <div className="h-[95vh] w-[25vw] absolute left-3 top-5 z-1000 bg-none rounded-md flex place-items-center justify-center">
+            <h1 className='text-4xl font-mono'>PodCraft</h1>
+
+        </div>
     </main>
   )
 }
